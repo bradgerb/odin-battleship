758a9c53e51c32a8fbba365c9421beb2dcd40d1c
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-06-09 18:01:49 -0400

    Test and write code for Ship class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-console.log('hello world')
-
 function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
+
+class Ship {
+    constructor(length) {
+        this.length = length;
+        this.hits = 0;
+    }
+
+    hit() {
+        this.hits++;
+        return this.hits
+    }
+
+    isSunk() {
+        if (this.hits >= this.length) {
+            return true
+        }
+        return false
+    }
+}
+
+module.exports = { sum, Ship }
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,33 @@
-const sum = require('./index');
+const { sum, Ship } = require('./index');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('constructor should set ship length', () => {
+    const battleship = new Ship(5);
+    expect(battleship.length).toBe(5);
+});
+
+test('hit() should increase hits by 1', () => {
+    const battleship = new Ship(5);
+    battleship.hit();
+    battleship.hit();
+    expect(battleship.hits).toBe(2);
+});
+
+test('isSunk should return true after hits >= length, check false condition', () => {
+    const battleship = new Ship(5);
+    for(let i = 0; i < 4; i++) {
+        battleship.hit();
+    }
+    expect(battleship.isSunk()).toBe(false);
+});
+
+test('isSunk should return true after hits >= length, check true condition', () => {
+    const battleship = new Ship(5);
+    for(let i = 0; i < 5; i++) {
+        battleship.hit();
+    }
+    expect(battleship.isSunk()).toBe(true);
 });
