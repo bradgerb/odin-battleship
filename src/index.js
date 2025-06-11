0b3b9cf44c920e606542d897d43a2b8bade48ee7
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-06-11 13:24:51 -0400

    Write tests for hits on placed ships and Gameboard constructor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-function sum(a, b) {
-  return a + b;
-}
-
 class Ship {
     constructor(length) {
         this.length = length;
@@ -21,4 +17,58 @@ class Ship {
     }
 }
 
-module.exports = { sum, Ship }
+class Gameboard {
+    constructor() {
+        this.rows = 10;
+        this.columns = 10;
+        this.board = [];
+
+        for (let i = 0; i < this.rows; i++) {
+            this.board[i] = new Array(this.columns).fill(null)
+        }
+    }
+
+    placeShip(name, [rowStart, columnStart], [rowEnd, columnEnd]) {
+        let shipLength = 0;
+        let orientation = 'none';
+        
+        if(this.#checkValidPlacement([rowStart, columnStart], [rowEnd, columnEnd])){            
+            if (rowStart === rowEnd) {
+                orientation = 'horizontal';
+                shipLength = Math.abs(columnEnd - columnStart) + 1;
+            } else {
+                orientation = 'vertical';
+                shipLength = Math.abs(rowEnd - rowStart) + 1;
+            };
+
+            name = new Ship(shipLength);
+
+            if (orientation === 'horizontal') {
+                for (let i = 0; i < shipLength; i++) {
+                    this.board[rowStart][columnStart + i] = name;
+                }
+            } else {
+                for (let i = 0; i < shipLength; i++) {
+                    this.board[rowStart + i][columnStart] = name;
+                }
+            }
+            
+        };
+    }
+
+    #checkValidPlacement([rowStart, columnStart], [rowEnd, columnEnd]) {
+        if (rowStart < 0 || columnStart < 0 || rowEnd < 0 || columnEnd < 0) {
+            return false
+        }
+        if (rowStart > this.rows || columnStart > this.columns || rowEnd > this.rows || columnEnd > this.columns) {
+            return false
+        }
+        if (rowStart != rowEnd && columnStart != columnEnd) {
+            return false
+        }
+        return true
+    }
+}
+
+// eslint-disable-next-line no-undef
+module.exports = { Ship, Gameboard }
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,6 @@
-const { sum, Ship } = require('./index');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const { Ship, Gameboard } = require('./index');
 
 test('constructor should set ship length', () => {
     const battleship = new Ship(5);
@@ -30,4 +28,30 @@ test('isSunk should return true after hits >= length, check true condition', () 
         battleship.hit();
     }
     expect(battleship.isSunk()).toBe(true);
+});
+
+test('check that hits register after placing ship on board', () => {
+    const battleship = new Ship(5);
+    const board = new Gameboard();
+    board.placeShip('battleship', [0, 0], [0, 4]);
+    board.board[0][3].hit();
+    expect(board.board[0][3].hits).toBe(1);
+});
+
+test('check that hits register for entire ship', () => {
+    const battleship = new Ship(5);
+    const board = new Gameboard();
+    board.placeShip('battleship', [0, 0], [0, 4]);
+    board.board[0][0].hit();
+    expect(board.board[0][3].hits).toBe(1);
+});
+
+test('check that hits register for only ship that was hit', () => {
+    const battleship = new Ship(5);
+    const destroyer = new Ship(4);
+    const board = new Gameboard();
+    board.placeShip('battleship', [0, 0], [0, 4]);
+    board.placeShip('destroyer', [1, 0], [1, 3]);
+    board.board[0][3].hit();
+    expect(board.board[1][3].hits).toBe(0);
 });
